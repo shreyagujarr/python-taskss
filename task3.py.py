f4eee70c53e06c65b3ff24444c81da047d606981
shreyagujarr <1032230895@tcetmumbai.in>
2025-05-04 01:20:33 +0530

    Assignment 2

---
+++ task3.py.py
@@ -0,0 +1,9 @@
+ 
+num = int(input("Enter an integer: "))
+
+
+if num % 2 == 0:
+    print(f"The number {num} is Even.")
+else:
+    print(f"The number {num} is Odd.")
+
---
+++ task4.py.py
@@ -0,0 +1,9 @@
+# Initialize sum variable
+total_sum = 0
+
+# Step 1 & 2: Use a for loop to iterate from 1 to 50 and calculate the sum
+for num in range(1, 51):
+    total_sum += num
+
+# Step 3: Display the final sum
+print(f"The sum of integers from 1 to 50 is: {total_sum}")
